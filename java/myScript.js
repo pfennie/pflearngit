5cab2febc799e595ccca1db25adfbfd773006af3
Patrick Fennie <pfennie@gmail.com>
2017-02-04 14:49:39 -0800

    Updated to joss site

--- java/myScript.js
+++
@@ -1,9 +0,0 @@
-function myFunction1() {
-	document.getElementById('demo1').src = 
-	'https://ih1.redbubble.net/image.24694638.0052/flat,800x800,070,f.u1.jpg';
-}
-
-function myFunction2() {
-	document.getElementById('demo2').src = 
-	'http://www.w3schools.com/js/pic_bulboff.gif';
-}
